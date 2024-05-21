035106720987f028aa0536ca43711a19b006d617
AlphaloopDotNet <alphaloop.firstmain@gmail.com>
2024-05-21 12:45:57 +0530

    change port

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Alphaloop</title>
+        <title>Alphaloop Portfolio</title>
         <link rel="icon" href="/img/favicon.ico" />
       </Head>
       <Cursor />
